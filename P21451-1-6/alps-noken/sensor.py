29cc4c6daa86ed426edcf12797f484ba5b5fd4fd
sonodaayu <sonoda@west.sd.keio.ac.jp>
2021-12-06 03:09:58 +0900

    updated sensor.py

--- P21451-1-6/alps-noken/sensor.py
+++ P21451-1-6/alps-noken/sensor.py
@@ -9,8 +9,10 @@ from datetime import datetime,date,timedelta
 from ftplib import FTP
 import time
 import sys #引数を受け取るために使う
+import requests
+import global_variable as g 
 
- 
+
 def s16(value):
     return -(value & 0b1000000000000000) | (value & 0b0111111111111111)
 
@@ -33,17 +35,19 @@ class NtfyDelegate(btle.DefaultDelegate):
             ack = int(cal[20:22], 16)
             #battery = int((cal[14:16] + cal[16:18]), 16)
             battery = int((cal[16:18] + cal[14:16]), 16)
-            hour_now=datetime.now().strftime("%Y-%m-%d-%H")
-            minute_now=datetime.now().strftime("%Y-%m-%d-%H-%M")
-            td=timedelta(hours=1)
-            trans_hour=datetime.now()-td
-            trans_hour=trans_hour.strftime("%Y-%m-%d-%H")
-            file_path='./data_save/battery/'+sensor_folder_name+hour_now+".json"
+#            if battery < 1500:
+#              params = {
+#                'message': "バッテリー残量が低下しています\n機体: {}\n現在の
+#                容量:{}mV".format(sensor_number, battery)
+#              }
+#              requests.post(g.LINE_URL, headers = g.headers, params = params)
 
-            server_path = 'SmaAgri/Noken/sonoda/'+sensor_folder_name
-            trans_file = trans_hour + '.json'
+
+            hour_now = datetime.now().strftime("%Y-%m-%d-%H")
+            minute_now = datetime.now().strftime("%Y-%m-%d-%H-%M")
+            trans_hour = (datetime.now() + timedelta(hours = -1)).strftime("%Y-%m-%d-%H") 
+            file_path = './data_save/battery/' + sensor_folder_name + hour_now +".json"
             path_battery = './data_save/battery/' + sensor_folder_name
-            trans_path = path_battery + trans_file
 
             if os.path.isfile(file_path):
                 with open(file_path,'r') as f:
@@ -57,27 +61,13 @@ class NtfyDelegate(btle.DefaultDelegate):
                     f.write('{}')
 
                 with open(file_path, 'r+') as f:
-                    all=json.load(f)
+                    all = json.load(f)
 
                 all[minute_now] = {"Battery":battery}
 
                 with open(file_path,'w') as f:
                     json.dump(all, f, indent=2)
 
-#                if os.path.isfile(trans_path):
-#                    ftp = FTP('10.26.0.1', 'ayu_ftp', passwd='WestO831')
-#                    with open(path_battery + trans_file, 'rb') as f:
-#                        ftp.storlines("STOR /"+server_path + trans_file, f)
-
-    #    if int((cal[0:2]), 16) == 0xf2:
-    #        GeoMagnetic_X = s16(int((cal[6:8] + cal[4:6]), 16)) * 0.15
-    #        GeoMagnetic_Y = s16(int((cal[10:12] + cal[8:10]), 16)) * 0.15
-    #        GeoMagnetic_Z = s16(int((cal[14:16] + cal[12:14]), 16)) * 0.15
-    #        print('Geo-Magnetic X:{0:.3f} Y:{1:.3f} Z:{2:.3f}'.format(GeoMagnetic_X, GeoMagnetic_Y, GeoMagnetic_Z))
-    #        Acceleration_X = 1.0 * s16(int((cal[18:20] + cal[16:18]), 16)) / 1024
-    #        Acceleration_Y = 1.0 * s16(int((cal[22:24] + cal[20:22]), 16)) / 1024
-    #        Acceleration_Z = 1.0 * s16(int((cal[26:28] + cal[24:26]), 16)) / 1024
-    #        print('Acceleration X:{0:.3f} Y:{1:.3f} Z:{2:.3f}'.format(Acceleration_X, Acceleration_Y, Acceleration_Z))
 
         if int((cal[0:2]), 16) == 0xf3:
             Pressure = int((cal[6:8] + cal[4:6]), 16) * 860.0/65535 + 250	
@@ -89,12 +79,10 @@ class NtfyDelegate(btle.DefaultDelegate):
             print('UV:{0:.3f} AmbientLight:{1:.3f} '.format(UV, AmbientLight))
 
 
-            hour_now=datetime.now().strftime("%Y-%m-%d-%H")
-            td=timedelta(hours=1)
-            trans_hour=datetime.now()-td
-            minute_now=datetime.now().strftime("%Y-%m-%d-%H-%M")
+            hour_now = datetime.now().strftime("%Y-%m-%d-%H")
+            minute_now = datetime.now().strftime("%Y-%m-%d-%H-%M")
 
-            file_path='./data_save/environment/' + sensor_folder_name+hour_now+".json"
+            file_path='./data_save/environment/' + sensor_folder_name + hour_now + ".json"
 
             if os.path.isfile(file_path):
                 with open(file_path,'r') as f:
@@ -112,21 +100,19 @@ class NtfyDelegate(btle.DefaultDelegate):
                     json.dump(all, f, indent=2)
 
                 ftp = FTP('10.26.0.1','ayu_ftp',passwd='WestO831')
-                hour = datetime.now().strftime('%Y-%m-%d-%H')
-                path_environment = './data_save/environment/' + sensor_folder_name
-                server_path = 'SmaAgri/Noken/sonoda/'+sensor_folder_name
-                trans_hour = trans_hour.strftime("%Y-%m-%d-%H")
-                trans_file_path = './data_save/environment/' + sensor_folder_name+trans_hour+'.json'
-                trans_file = trans_hour+'.json'
-                if os.path.isfile(trans_file_path):
+                last_hour = (datetime.now() + timedelta(hours = -1)).strftime('%Y-%m-%d-%H')
+                trans_file_path = './data_save/environment/' + sensor_folder_name + last_hour + '.json'
+                qnap_path = 'SmaAgri/Noken/sonoda/' + sensor_folder_name + last_hour + '.json'
 
+                if os.path.isfile(trans_file_path):
                     with open(trans_file_path, 'rb') as f:
-                        ftp.storlines("STOR "+server_path + trans_file, f)
+                        ftp.storlines("STOR /"+ qnap_path, f)
              
+
                 ftp_takayama = FTP('192.168.11.4', 'sonoda', passwd='WestO831')
                 if os.path.isfile(trans_file_path):
                     with open(trans_file_path, 'rb') as f:
-                        ftp_takayama.storlines("STOR "+"/mnt/ssd/sonoda/" + sensor_folder_name + trans_file, f)
+                        ftp_takayama.storlines("STOR /mnt/ssd/sonoda/" + "sensor" + sensor_number + "/" + last_hour + '.json', f)
 
 
 
@@ -137,41 +123,50 @@ class AlpsSensor(Peripheral):
  
 
 def main(sensor_number):
-        sensor_list = ['98','81','70','8E','8D','6E','6F']
-        sensor_list = ["48:F0:7B:78:4B:" + x for x in sensor_list]
+  while True:
+    try:
 
-        sensor =sensor_list[int(sensor_number)-1]
+      sensor_list = ['98','81','70','8E','8D','6E','6F']
+      sensor_list = ["48:F0:7B:78:4B:" + x for x in sensor_list]
 
-        alps = AlpsSensor(sensor)
-        alps.setDelegate( NtfyDelegate(btle.DefaultDelegate) )
- 
-        alps.writeCharacteristic(0x0013, struct.pack('<bb', 0x01, 0x00), True)# Custom1 Notify Enable 
-        alps.writeCharacteristic(0x0016, struct.pack('<bb', 0x01, 0x00), True)# Custom2 Notify Enable
+      sensor =sensor_list[int(sensor_number)-1]
 
-     
-        alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x2F, 0x03, 0x03), True) # (不揮発)保存内容の初期化
-        alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x01, 0x03, 0x7c), True) # 地磁気、加速度,気圧,温度,湿度,UV,照度を有効
-        alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x04, 0x03, 0x00), True) # slow Mode
+      alps = AlpsSensor(sensor)
+      alps.setDelegate( NtfyDelegate(btle.DefaultDelegate) )
+
+      alps.writeCharacteristic(0x0013, struct.pack('<bb', 0x01, 0x00), True)# Custom1 Notify Enable 
+      alps.writeCharacteristic(0x0016, struct.pack('<bb', 0x01, 0x00), True)# Custom2 Notify Enable
+
+
+      alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x2F, 0x03, 0x03), True) # (不揮発)保存内容の初期化
+      alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x01, 0x03, 0x7c), True) # 地磁気、加速度,気圧,温度,湿度,UV,照度を有効
+      alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x04, 0x03, 0x00), True) # slow Mode
 #    alps.writeCharacteristic(0x0018, struct.pack('<bbbb', 0x06, 0x04, 0x64, 0x00), True) # Fast 100msec (地磁気,加速度)
 #    alps.writeCharacteristic(0x0018, struct.pack('<bbbb', 0x06, 0x04, 0x7A, 0x01), True) # Fast 250msec (地磁気,加速度)
-        alps.writeCharacteristic(0x0018, struct.pack('<bbbb', 0x05, 0x04, 0x3C, 0x00), True) # Slow 1sec (気圧,温度,湿度,UV,照度)
+      alps.writeCharacteristic(0x0018, struct.pack('<bbbb', 0x05, 0x04, 0x3C, 0x00), True) # Slow 1sec (気圧,温度,湿度,UV,照度)
 
 #    alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x21, 0x03, 0x01), True)
- #   alps.writeCharacteristic(0x0018, struct.pack('<bbbb', 0x10, 0x04, 0x01, 0x00), True)
+#   alps.writeCharacteristic(0x0018, struct.pack('<bbbb', 0x10, 0x04, 0x01, 0x00), True)
 
-        alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x24, 0x03, 0x01), True) #自動status通知
+      alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x24, 0x03, 0x01), True) #自動status通知
 
-        #alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x02, 0x03, 0x02), True) # 加速度±8G
-        alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x2F, 0x03, 0x01), True) # 設定内容保存
-        alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x20, 0x03, 0x01), True) # センサ計測開始
-     
+#alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x02, 0x03, 0x02), True) # 加速度±8G
+      alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x2F, 0x03, 0x01), True) # 設定内容保存
+      alps.writeCharacteristic(0x0018, struct.pack('<bbb', 0x20, 0x03, 0x01), True) # センサ計測開始
+
 # Main loop --------
-        while True:
-            if alps.waitForNotifications(1.0):
-            # handleNotification() was called
-                continue
-            print('waiting...')
-        # Perhaps do something else here
+      while True:
+          if alps.waitForNotifications(1.0):
+          # handleNotification() was called
+              continue
+          print('waiting...')
+# Perhaps do something else here
+    
+    except Exception as e:
+      params = {'message' : "Error occured on sensor{}.{}".format(sensor_number, str(e))}
+      requests.post(g.LINE_URL, headers = g.headers, oparams = params)
+
+
 if __name__ == "__main__":
     args = sys.argv
     sensor_number = args[1]
