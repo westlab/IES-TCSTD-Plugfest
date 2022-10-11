836f820286714e7e68a996691f4336d1c27180bf
Akihito Nishikawa <akihito.n.0910@keio.jp>
2022-10-11 16:49:46 +0900

    リクエストに対する条件分岐周りの追加

--- NetworkService/NCAPnet.py
+++ NetworkService/NCAPnet.py
@@ -7,10 +7,13 @@ import uuid
 import gmqtt
 
 from NCAPlib import Tpl2Msg
-# from NCAPsm import MBRTable
+from NCAPOP import confread
+from NCAPsm import MBRtable
 
 addr = uuid.UUID(int=uuid.getnode()).hex[-12:] + "mqtt"
 client = gmqtt.Client(addr)
+
+mbrTable = MBRtable()
 
 def on_connect(client, flags, rc, properties):
     print('[CONNECTED {}]'.format(client._client_id))
@@ -26,6 +29,67 @@ def on_message(client, topic, payload, qos, properties):
     print('[RECV MSG {}] TOPIC: {} PAYLOAD: {} QOS: {} PROPERTIES: {}'
         .format(client._client_id, topic, payload, qos, properties))
 
+    ### TODO implement ParseRequest
+
+    ### TODO implement if statement
+    if req == reqDiscovery:
+        descovery()
+    elif req == reqSecession:
+        secession()
+    elif req == reqSyncAccess:
+        syncAccess()
+    elif req == reqAsyncAccess:
+        asyncAccess()
+    elif req == reqAsyncTerminate:
+        asyncTerminate()
+    else:
+        raise Exception("Illigal reqest")
+
+def descovery(self) -> None:
+    """
+    Add app to appTable.
+    """
+    pass
+
+def secession(self) -> None:
+    """
+    Delete app from appTable
+    """
+    pass
+
+def syncAccess(self) -> None:
+    """
+    Process a request for SyncRead
+    """
+    pass
+
+def asyncAccess(self) -> None:
+    """
+    Add app to asyncList
+    """
+    pass
+    
+def asyncTerminate(self) -> None:
+    """
+    Remove app from asyncList
+    """
+    pass
+
+def getAsyncList(self) -> list:
+    """
+    get list of apps which subscribe AsyncRead
+    """
+    pass
+    
+def asyncPublish(self) -> None:
+    """
+    Publish sensor data to clients in AsyncList
+    """
+    asyncList = self.getAsyncList()
+    for app in asyncList:
+        ### TODO implement publish sensor data to apps
+        pass
+
 async def start(host, port, subscriptor):
     await client.connect(host, port)
     client.subscribe(subscriptor, subscription_identifier=len(subscriptor))
@@ -33,7 +97,8 @@ async def start(host, port, subscriptor):
 
 if __name__ == "__main__":
     print("demonstration start")
-
+    # confread()
+    
     print('MQTT client setup. Client ID:', addr)
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
@@ -44,6 +109,7 @@ if __name__ == "__main__":
         gmqtt.Subscription("hoge", qos=2), gmqtt.Subscription("fuga", qos=2)
     ]
 
+    print("waiting access from app")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start("localhost", "1883", SUBSCRIPTOR))
     
